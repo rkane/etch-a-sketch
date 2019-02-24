9f9b81c4ed9571ac70a093f0b33bfc1dce4b5248
Garan Weber <garanw@gmail.com>
2019-02-23 19:23:05 -0600

    Changed to flex box method. Added variable sizing

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 
 function addClass(e){
-    console.log(e);
     e.target.classList.add('hovered');
 }
 
+
+
 function createGrid(size){
     const board = document.querySelector("#gameBoard");
-    for (i=0; i<size; i++){
-        const row = document.createElement("div");
-        for(j=0; j<size; j++){
-            const box = document.createElement("div");
-            box.classList.add("squareBox");
-            row.appendChild(box);
-        }
-        board.appendChild(row);
+    while (board.hasChildNodes()){
+        board.removeChild(board.childNodes[0]);
+    };
+    for (i=0; i<(size * size); i++){
+        const dimension = ((800/size) - 2) + "px";
+        const box = document.createElement("div");
+        box.classList.add("squareBox");
+        box.setAttribute("style", `height:${dimension}; width:${dimension}`)
+        board.appendChild(box);
+         
     }
     const squareBoxes = Array.from(document.querySelectorAll(".squareBox"));
     squareBoxes.forEach(squareBox => squareBox.addEventListener('mouseover', addClass));
 }
 
+function shake(){
+    const squareBoxes = Array.from(document.querySelectorAll(".squareBox"));
+    squareBoxes.forEach(squareBox => squareBox.classList.remove("hovered"));
+    let size = prompt("What size?");
+    createGrid(size);
+}
 
 
